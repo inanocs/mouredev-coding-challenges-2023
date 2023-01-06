e6352c633e33ff3183b9edc91b35ea88a827717a
inanocs <nanocabezasanchez@gmail.com>
2023-01-06 01:05:16 +0100

    refactor: challenge-0 test cases

--- challenges/challenge-0/test/fizzbuzz.test.js
+++ challenges/challenge-0/test/fizzbuzz.test.js
@@ -1,31 +1,31 @@
 import { fizzBuzz } from 'challenge-0/src/fizzbuzz'
+const jTest = test
 
-describe('Challenge 0', () => {
-  const filterCondition = (num, expectedResult) =>
-    fizzBuzz(num) === expectedResult
-  test('fizz response', () => {
-    const testSamples = [3, 6, 9]
-    const expectedResult = 'fizz'
-    const results = testSamples.filter((num) =>
-      filterCondition(num, expectedResult)
-    ).length
-    expect(results).toBe(testSamples.length)
-  })
-  test('buzz response', () => {
-    const testSamples = [5, 10, 25]
-    const expectedResult = 'buzz'
-    const results = testSamples.filter((num) =>
-      filterCondition(num, expectedResult)
-    ).length
-    expect(results).toBe(testSamples.length)
-  })
+describe('Challenge 0 - FizzBuzz', () => {
+  const testCases = [
+    {
+      name: 'fizz response',
+      sample: [3, 6, 9],
+      expectedResult: 'fizz',
+    },
+    {
+      name: 'buzz response',
+      sample: [5, 10, 25],
+      expectedResult: 'buzz',
+    },
+    {
+      name: 'fizzbuzz response',
+      sample: [15, 30, 45],
+      expectedResult: 'fizzbuzz',
+    },
+  ]
+  testCases.forEach((testCase) => {
+    const { name, sample, expectedResult } = testCase
+    const filterCondition = (num) => fizzBuzz(num) === expectedResult
+    test(name, () => {
+      const resultsLength = sample.filter(filterCondition).length
 
-  test('fizzbuzz response', () => {
-    const testSamples = [15, 30, 45]
-    const expectedResult = 'fizzbuzz'
-    const results = testSamples.filter((num) =>
-      filterCondition(num, expectedResult)
-    ).length
-    expect(results).toBe(testSamples.length)
+      expect(resultsLength).toBe(sample.length)
+    })
   })
 })
